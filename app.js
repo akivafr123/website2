39576675ac8844ea99a5a6006c3b0a27438def23
Akiva Friedman-Rosner <arosner.akiva@gmail.com>
2016-08-23 21:13:03 -0400

    add ability to add a preselected new comment

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ var main = function() {
 	"use strict";
 
 	$(".comment-input button").on("click", function(event) {
-		console.log("Hello World!");
+		$(".comments").append("<p>this is a new comment</p>");
 	});
 };
 
 $(document).ready(main);
 
-// https://github.com/Microsoft/PTVS/wiki/Selecting-and-Installing-Python-Interpreters
 
-// http://ptvs.azurewebsites.net/news/
