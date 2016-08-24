775d4356a6084e316444de79fa8e433804b59b5c
Akiva Friedman-Rosner <arosner.akiva@gmail.com>
2016-08-23 21:02:10 -0400

    add styling to button and page background. misc improvements

---
+++ app.js
@@ -0,0 +1,13 @@
+var main = function() {
+	"use strict";
+
+	$(".comment-input button").on("click", function(event) {
+		console.log("Hello World!");
+	});
+};
+
+$(document).ready(main);
+
+// https://github.com/Microsoft/PTVS/wiki/Selecting-and-Installing-Python-Interpreters
+
+// http://ptvs.azurewebsites.net/news/
